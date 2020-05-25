0f7a96d47288ebe0c5328ff70b80963bed44019f
PierreSimT <alu0101371030@ull.edu.es>
2020-05-25 18:34:29 +0100

    Arreglando Cypress: Cambios menores

--- cypress/integration/profile-spec.js
+++ cypress/integration/profile-spec.js
@@ -40,28 +40,4 @@ describe('Profile', () => {
       .should('have.text', 'my new bio')
   })
 
-  it('shows favorite articles', () => {
-    cy.article({
-      title: 'first post',
-      description: 'first description',
-      body: 'first article',
-      tagList: ['first', 'testing']
-    })
-
-    cy.location('pathname').should('include', '/article/first-post')
-    cy.get('[data-cy=home]').click()
-    cy.get('[data-cy=global-feed]').click()
-    cy.get('.article-preview')
-      .should('have.length', 1)
-      .first()
-      .find('[data-cy=fav-article]')
-      .click()
-      // check if the click has registered and the favorite flag has been set
-      .should('not.have.class', 'btn-outline-primary')
-
-    // check favorited articles feed
-    cy.get('[data-cy=profile]').click()
-    cy.get('[data-cy=favorited-articles]').click()
-    cy.get('.article-preview').should('have.length', 1)
-  })
 })
